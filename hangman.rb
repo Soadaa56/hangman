87ac73586f63ea8139bc2f33f655af28cd2a5693
soadaa <Soadhlh@gmail.com>
2022-11-11 18:17:10 -0500

    Intro about project; Instructions

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,7 @@
 #hangman.rb
-# puts "Lets play a game of Hangman!"
-
 file = File.open('google-10000-english-no-swears.txt')
 @word_list = file.readlines.map(&:chomp)
+
 def random_word
   word = @word_list[(rand*10000).round]
   if word.length >= 5 && word.length <= 12
@@ -10,6 +9,9 @@ def random_word
   else
     random_word
   end
-end 
+end
+
 puts random_word
+
+
 file.close
