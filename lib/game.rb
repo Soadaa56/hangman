3f6750716117ef56abf606977c1f63ed6cc50d81
soadaa <Soadhlh@gmail.com>
2022-11-11 19:43:39 -0500

    Formatted into class Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,22 @@
-#game.rb
-file = File.open('google-10000-english-no-swears.txt')
-@word_list = file.readlines.map(&:chomp)
+# game.rb
+require 'pry-byebug'
 
-def random_word
-  word = @word_list[(rand*10000).round]
-  if word.length >= 5 && word.length <= 12
-    word
-  else
-    random_word
+class Game
+  def initialize
+    @secret_word = generate_word
   end
+
+  def generate_word
+    file = File.open('google-10000-english-no-swears.txt', 'r')
+    dictionary = file.readlines.map(&:chomp)
+    word = dictionary[(rand*10000).round]
+    if word.length >= 5 && word.length <= 12
+      word
+    else
+      generate_word
+    end
+    file.close
+  end
+
+  puts @secret_word
 end
-
-puts random_word
-
-
-file.close
