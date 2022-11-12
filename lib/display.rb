caf91822a9850128de54bbc911a2c3fe0927f5b1
soadaa <Soadhlh@gmail.com>
2022-11-12 16:20:34 -0500

    Display instructions of game when starting

---
+++ lib/display.rb
@@ -0,0 +1,15 @@
+# display.rb
+
+module Display
+  def display_instructions
+    <<~HEREDOC
+
+      Instructions for Hangman:
+
+      A random word will be chosen with a length of 5-12 letters. On each turn you will guess one letter and if the letter is a part of the random word it will be 
+      shown to you. You will keep guessing letters until you find each letter of the random word or you run out of guesses (7).
+
+    HEREDOC
+  end
+
+end
