4d7a79dd4a216f7cb00b7f8459a0c234eefe8c21
soadaa <Soadhlh@gmail.com>
2022-11-14 10:44:52 -0500

    Fix generate_word function

--- lib/game.rb
+++ lib/game.rb
@@ -5,37 +5,38 @@ require 'pry-byebug'
 
 class Game
   attr_accessor :word, :available_letters, :solved_letter, :incorrect_letters
+  
   include Display
 
   def initialize
     @available_letters = ('a'..'z').to_a
-    @solved_letter = Array.new
-    @incorrect_letters = Array.new
+    @solved_letter = []
+    @incorrect_letters = []
     start_game
   end
 
   def start_game
     puts display_instructions
     new_game
+    # Start new game on 1
+    # Load new game on 2
   end
 
   def new_game
     @word = generate_word
+    # remove print from next line after debugging
     p @word
   end
 
   def generate_word
-    file = File.open('google-10000-english-no-swears.txt', 'r')
-    dictionary = file.readlines.map(&:chomp)
-    word = dictionary[(rand*10000).round]
-    if word.length >= 5 && word.length <= 12
-      word
-    else
-      generate_word
+    dictionary = File.open('google-10000-english-no-swears.txt', 'r').readlines
+    @valid_words = []
+    dictionary.each do |word|
+      @valid_words << word.strip!
     end
-    file.close
+    @valid_words = @valid_words.each.select { |word| word.length.between?(5,12) }
+    @valid_words[rand(0..@valid_words.length)]
   end
-
 
 end
 
